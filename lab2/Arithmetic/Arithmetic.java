a17c432f44a1d9e1feef8bbf447f74af291657fb
Weixiao Chen <weixiao.chen.98@foxmail.com>
2025-05-22 16:31:16 +0800

    lab2 done

--- lab2/Arithmetic/Arithmetic.java
+++ lab2/Arithmetic/Arithmetic.java
@@ -20,6 +20,28 @@ public class Arithmetic {
      * @return Sum of a and b
      * */
     public static int sum(int a, int b) {
-        return a * b;
+        return a + b;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i], minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+
+            if (min != arr[i]) {
+                swap(arr, i, minIndex);
+            }
+        }
     }
 }
